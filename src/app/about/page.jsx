32d3e47c6e2423351fb77ef3ffb0abf5e92d3031
Maulana Ihsan <smamalik33@gmail.com>
2024-05-29 20:29:56 +0700

    add: create navbar component

---
+++ src/app/about/page.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4 p-3">
+      <h1 className="text-2xl font-medium text-amber-500 ">About</h1>
+      <p>
+        Welcome to our movie database website! We are a team of passionate movie
+        enthusiasts who have come together to create a one-stop destination for
+        all your movie-related needs. Our website is designed to provide you
+        with a comprehensive database of movies from all around the world, along
+        with the latest news, reviews, and trailers. Our movie database is
+        constantly updated with new releases, ensuring that you have access to
+        the latest and greatest in the world of cinema. You can search for
+        movies by title, director, actor, genre, or release date, making it easy
+        to find the perfect movie for any occasion.
+      </p>
+      <p>
+        Welcome to our movie database website! We are a team of passionate movie
+        enthusiasts who have come together to create a one-stop destination for
+        all your movie-related needs. Our website is designed to provide you
+        with a comprehensive database of movies from all around the world, along
+        with the latest news, reviews, and trailers. Our movie database is
+        constantly updated with new releases, ensuring that you have access to
+        the latest and greatest in the world of cinema. You can search for
+        movies by title, director, actor, genre, or release date, making it easy
+        to find the perfect movie for any occasion.
+      </p>
+      <p>
+        Welcome to our movie database website! We are a team of passionate movie
+        enthusiasts who have come together to create a one-stop destination for
+        all your movie-related needs. Our website is designed to provide you
+        with a comprehensive database of movies from all around the world, along
+        with the latest news, reviews, and trailers. Our movie database is
+        constantly updated with new releases, ensuring that you have access to
+        the latest and greatest in the world of cinema. You can search for
+        movies by title, director, actor, genre, or release date, making it easy
+        to find the perfect movie for any occasion.
+      </p>
+    </div>
+  );
+}
